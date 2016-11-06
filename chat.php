da9fe3ab38764fc86e16bf58d60a468ed265c06d
DarkFate13 <ganesh3597@gmail.com>
2016-11-06 16:28:35 +0600

    Final UI
    Full UI done

--- chat.php
+++ chat.php
@@ -5,13 +5,17 @@
         <title>Chatit</title>
         <link rel="stylesheet" type="text/css" href="./css/normalize.css">
         <link rel="stylesheet" type="text/css" href="./css/style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">        
         <script type="text/javascript" src="./js/chat.js"></script>
     </head>
     <body>
-        <p>
-            Hey, <big><b><?php echo $_SESSION['user_name']; ?></b></big>. You are logged in. Try to close this browser tab and open it again. Still logged in! ;)
-        </p>
-        <a href="index.php?logout">Logout</a>
+    	<br/>
+        <p align="center">
+            Hey, <big><b> username <!--<?php echo $_SESSION['user_name']; ?> -->  </b></big>. You are logged in. Try to close this browser tab and open it again. Still logged in! ;)
+            &nbsp; &nbsp;
+			<button type="button" class="btn btn-primary" >LOG OUT</button>
+
+       </p>
         <div class="ui">
             <div class="left-menu">
                 <form action="#" class="search">
@@ -22,11 +26,13 @@
             </div>
             <div class="chat">
                 <div class="top">
-                    <div class="avatar">
-                        <img class="images" src="./img/dp3.jpg">
+                    <div class="avatar"  >
+                        <img class="images" src="./img/dp3.jpg" style = "margin-top: -70px;">
                     </div>
-                    <div class="info">
-                        <div class="name">Daniel Isaac</div>
+                    <div class="info" >
+                        <div class="name" style = "margin-top: 25px;">Daniel Isaac
+                        	<a href="" onclick="prompt('Enter email id')";> <img src='./img/ic_email_black_24dp_2x.png' style = "margin-left: 380px; margin-top: -35px;"  /> </a>
+                        </div>
                     </div>
                     <i class="fa fa-star"></i>
                 </div>
